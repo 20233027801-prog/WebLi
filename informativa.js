9ab6cf138f74589d18f1ba6ca318cc2abdf27a99
Yamim Bruch <20233027801@estudantes.ifpr.edu.br>
2025-11-13 16:09:27 -0300

    Atualização do site

--- informativa.js
+++ informativa.js
@@ -1,23 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  alert("Bem-vindo(a) à página informativa sobre LIBRAS!");
-
-  const links = document.querySelectorAll("nav a");
-
-  links.forEach(link => {
-    link.addEventListener("click", function (event) {
-      event.preventDefault(); 
-
-      links.forEach(l => l.classList.remove("ativo"));
-
-      this.classList.add("ativo");
-
-      if (this.textContent.includes("Cadastre-se")) {
-        alert("Em breve você poderá se cadastrar!");
-      } else if (this.textContent.includes("Login")) {
-        alert("Área de login em desenvolvimento!");
-      } else if (this.textContent.includes("Início")) {
-        alert("Você já está na página inicial!");
-      }
-    });
-  });
-});
+function irparalogin(params) {
+  window.location.href = "loguin.html"
+}
