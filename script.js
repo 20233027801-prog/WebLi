696c5117bbcff68c65a17e04619759ecc9d4473e
Yamim Bruch <20233027801@estudantes.ifpr.edu.br>
2025-11-14 13:40:32 -0300

    Atualização do site

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function irparalogin() {
   window.location.href = "login.html";
 }
+function irparaInicio() {
+  window.location.href = "index.html"; // sua página inicial
+}
+
+function irparaCadastro() {
+  window.location.href = "cadastro.html"; // coloque o nome da página de cadastro
+}
+
+function irparaLogin() {
+  window.location.href = "login.html"; // coloque o nome da página de login
+}
